763e0341fd855700974bdc7c8ee307d266069e48
Alexis Emerson <alexis.g.emerson@gmail.com>
2025-03-15 14:53:03 -0400

    added labels

--- src/_components/Header.jsx
+++ src/_components/Header.jsx
@@ -1,7 +1,21 @@
 export default function Header() {
     return (
-        <header className="">
-            
+        <header className="relative">
+            <div className="bg-pink-300 flex flex-row justify-between h-16">
+            <div>
+                <a className="">
+                <img src="/assets/placeholderlogo.png" className="h-8">
+                </img>
+                </a>
+            </div>
+            <div> 
+                <a>About</a>
+                <a>FAQs</a>
+                <a>Manuals</a>
+                <a>Videos</a>
+                <a>Resources & Tools</a>
+            </div>
+            </div>
         </header>
     );
 }
