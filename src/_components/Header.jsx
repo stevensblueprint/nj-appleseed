6a96e9436ea9b436ea7df2abda9fab3dc4f56242
Alexis Emerson <alexis.g.emerson@gmail.com>
2025-03-17 19:54:05 -0400

    added svgs, links

--- src/_components/Header.jsx
+++ src/_components/Header.jsx
@@ -4,16 +4,23 @@ export default function Header() {
             <div className="bg-pink-300 flex flex-row justify-between h-16">
             <div>
                 <a className="">
-                <img src="/assets/placeholderlogo.png" className="h-8">
+                <img src="/assets/placeholderlogo.png" alt="NJAppleseed Logo" width="300" height="500">
                 </img>
                 </a>
             </div>
             <div> 
-                <a>About</a>
-                <a>FAQs</a>
-                <a>Manuals</a>
-                <a>Videos</a>
-                <a>Resources & Tools</a>
+                <a href="/about">About</a>
+                <a href="/faqs">FAQs</a>
+                <a href="/manual">Manuals</a>
+                <a href="/videos">Videos</a>
+                <a href="/resources">Resources & Tools</a>
+            </div>
+            <div> 
+                <a>
+                <svg className="w-10 h-10 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6 h16 M4 12 h16 M4 18 h16"></path>
+                </svg>
+                </a>
             </div>
             </div>
         </header>
