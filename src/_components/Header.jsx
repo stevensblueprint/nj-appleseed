41dd3dcb2e20105e49cb0b8c63f174e8f89f8627
Alexis Emerson <alexis.g.emerson@gmail.com>
2025-03-25 22:50:56 -0400

    quick fixes to logo size

--- src/_components/Header.jsx
+++ src/_components/Header.jsx
@@ -16,10 +16,10 @@ export default function Header() {
                 title="Mobile Menu"
                 className="hidden peer"
                 />
-            <div className="flex flex-row justify-between p-6">
+            <div className="flex flex-row justify-between p-6 items-center">
             <div>
                 <a href="/">
-                <img src="/assets/image.png" alt="Placeholder Logo"></img>
+                <img src="/assets/image.png" alt="Placeholder Logo" className="max-h-24 max-w-36 md:max-w-48 md:max-h-24"></img>
                 </a>
             </div>     
             <div className="md:hidden"> 
