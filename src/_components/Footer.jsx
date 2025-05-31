9e4b29d85019c8b5d44f4adda988db4140d69a7c
Christian Apostol <christiand.apostol@outlook.com>
2025-05-31 17:48:10 -0400

    basic footer content

--- src/_components/Footer.jsx
+++ src/_components/Footer.jsx
@@ -1,11 +1,43 @@
 export default function Footer() {
+  // TODO: Make a global import for both Header and Footer
+  const navigationLinks = [
+    { href: "/about", text: "ABOUT" },
+    { href: "/faqs", text: "FAQs" },
+    { href: "/manual", text: "MANUAL" },
+    { href: "/videos", text: "VIDEOS" },
+    { href: "/resources", text: "RESOURCES & TOOLS" },
+    { href: "/practices", text: "BEST PRACTICES" },
+  ];
+
   return (
-    <footer className="bg-footer-grey">
-      <div>
-        Logo + Disclaimer
+    <footer className="flex bg-footer-grey">
+      <div className="flex flex-col">
+        <img
+          src="/assets/EndMedDebtLogo.png"
+          alt="End Medical Debt Logo"
+          className="max-h-24 max-w-36 md:max-w-48 md:max-h-24"
+        ></img>
+        <p>
+          DISCLAIMER: The information on this website and in the Manual is not
+          legal advice. It is a source of information to help New Jerseyans deal
+          with medical debt. For legal advice, consult an attorney. 
+        </p>
       </div>
-      <div>
-        Links + Copyright
+      <div className="flex flex-col">
+        <div>
+          {navigationLinks.map((link) => (
+            <a
+              key={link.href}
+              href={link.href}
+              className="hover:text-gray-300 font-bold transition-colors"
+            >
+              {link.text}
+            </a>
+          ))}
+        </div>
+        <p>
+          New Jersey Appleseed Public Law Center @2025. All rights reserved.
+        </p>
       </div>
     </footer>
   );
