39c0baa5e399e68633c9259ddfdc11807e820e86
Christian Apostol <christiand.apostol@outlook.com>
2025-06-01 13:06:24 -0400

    fixed mobile responsiveness and gradient for hero

--- src/_components/MainHero.jsx
+++ src/_components/MainHero.jsx
@@ -1,13 +1,15 @@
 export default function MainHero() {
   return (
     <header
-      className="relative w-full h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url(/assets/image.png)" }}
+      className="w-full h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url(/assets/stethoscope.png)" }}
     >
-      <div className="absolute inset-0 bg-gradient-to-r from-black via-black/70 to-transparent"></div>
-      <div className="relative z-10 flex flex-col justify-center items-start h-full p-10 text-white max-w-lg md:max-w-xl">
-        <p className="md:text-6xl text-5xl md:leading-snug font-black md:mx-6 max-w-5xl align-middle">
-          Empowering individuals to protect their financial well-being.
+      <div
+        className="flex flex-col justify-center items-start h-full w-full px-4 py-10 md:px-10 text-white max-w-lg md:max-w-xl
+        bg-gradient-to-r from-black from-30% via-black/80 via-80% to-transparent"
+      >
+        <p className="relative z-10 md:text-6xl text-3xl md:leading-snug font-black md:mx-6 max-w-full md:max-w-5xl align-middle">
+          Empowering individuals to address the challenge of Medical Debt.
         </p>
       </div>
     </header>
