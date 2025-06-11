895da79a25b7c8399796481429417e0831dff2a8
Alexis Emerson <alexis.g.emerson@gmail.com>
2025-06-11 17:44:13 -0400

    added tag to every page

--- src/_includes/_layouts/FAQs.jsx
+++ src/_includes/_layouts/FAQs.jsx
@@ -6,6 +6,22 @@ export default function FAQs({ title, faqs, text, comp }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
         <link rel="stylesheet" href="/css/styles.css" />
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-00LR9GZHMZ"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+          gtag("config", "G-00LR9GZHMZ");
+          `,
+          }}
+        />
       </head>
       <body className="bg-gray-100 min-h-screen">
         <comp.Header />
