ff74764e5ff3be9b4d7a81136e08a1068cbc65c9
Christian Apostol <94158310+Caposto@users.noreply.github.com>
2025-06-16 18:35:05 -0400

    Update/guide (#63)
    * added updated guide
    
    * added spanish consumer guide
    
    * implemented desktop design
    
    * implemented mobile designs
    
    * added attribute button

--- src/_components/ManualHero.jsx
+++ src/_components/ManualHero.jsx
@@ -1,42 +1,47 @@
 export default function ManualHero() {
   return (
     <header
-      className="relative h-[480px] md:h-screen bg-cover bg-center flex items-center justify-center"
-      style={{ backgroundImage: "url('/assets/image.png')" }}
+      className="relative w-full h-80 sm:h-screen bg-center bg-cover"
+      style={{ backgroundImage: "url('/assets/manual_hero.png')" }}
     >
-      <div className="z-10 flex flex-col p-10 text-black items-center">
-        <p className="md:text-7xl max-w-2xl font-normal text-5xl pb-8 text-center">
-          Explore our manual for resources on <b>medical debt.</b>
-        </p>
-        <div className="flex flex-col md:flex-row gap-4">
-          <a
-            href="/assets/docs/ConsumerMedicalDebtAppleseedManual.pdf" // add path to pdf manual
-            className="bg-primary hover:bg-secondary px-6 py-3 flex flex-row  items-center justify-center rounded-lg"
-            download={"Medical_Debt_Appleseed_Manual"} // add what we want the manual to be called when added
-          >
-            <svg
-              width="34"
-              height="34"
-              viewBox="0 0 34 34"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="px-2"
-            >
-              <path
-                d="M29.75 21.25V26.9167C29.75 27.6681 29.4515 28.3888 28.9201 28.9201C28.3888 29.4515 27.6681 29.75 26.9167 29.75H7.08333C6.33189 29.75 5.61122 29.4515 5.07986 28.9201C4.54851 28.3888 4.25 27.6681 4.25 26.9167V21.25M9.91667 14.1667L17 21.25M17 21.25L24.0833 14.1667M17 21.25V4.25"
-                stroke="white"
-                stroke-width="3.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-            <p className="text-white text-lg font-bold">DOWNLOAD A COPY</p>
-          </a>
-          <div className="bg-dark-green px-6 py-3 flex flex-row items-center justify-center rounded-lg">
-            <p className="text-white text-lg font-bold">English | ᐯ</p>
-          </div>
+      <div className="absolute inset-0 bg-black/40" />
+
+      <div className="relative z-10 flex flex-col justify-center items-center h-full text-center px-4">
+        <h1 className="text-white text-xl sm:text-3xl md:text-5xl lg:text-6xl font-bold mb-6 sm:mb-8 px-4 sm:px-16 lg:px-32">
+          Explore our manual for resources on <strong>medical debt.</strong>
+        </h1>
+        <div className="flex flex-col sm:flex-row gap-3 sm:gap-4 w-full sm:w-auto">
+          <DownloadButton
+            href="/assets/docs/ConsumerGuideEnglish.pdf"
+            text="DOWNLOAD A COPY | ENGLISH"
+            backgroundColor="bg-dark-green"
+            hoverColor="hover:bg-dark-green/80"
+          />
+          <DownloadButton
+            href="/assets/docs/ConsumerGuideSpanish.pdf"
+            text="DESCARGAR UNA COPIA | ESPAÑOL"
+            backgroundColor="bg-primary"
+            hoverColor="hover:bg-secondary"
+          />
         </div>
       </div>
     </header>
+  );
+}
+
+function DownloadButton({ href, text, backgroundColor, hoverColor }) {
+  return (
+    <a
+      href={href}
+      download
+      className={`${backgroundColor} ${hoverColor} text-white px-4 py-2 sm:px-6 sm:py-3 rounded-md flex items-center justify-center w-full sm:w-auto`}
+    >
+      <img
+        src="/assets/vector/download.svg"
+        className="w-5 h-5 pr-2"
+        alt="Download"
+      />
+      <strong className="text-sm sm:text-base">{text}</strong>
+    </a>
   );
 }
