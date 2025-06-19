bf68016970ac44e9d2fdda756990e55383e0635e
Christian Apostol <christiand.apostol@outlook.com>
2025-06-18 23:24:16 -0400

    finished desktop implementation

--- src/_components/ManualSectionDropdown.jsx
+++ src/_components/ManualSectionDropdown.jsx
@@ -1,6 +1,7 @@
 import ReactMarkdown from "react-markdown";
 
 export default function ManualSectionDropdown({ search }) {
+  // Retrieve every page in the 'manual' directory which is marked as type "section"
   const sections = search
     .pages("type=section")
     .map((section) => ({
@@ -8,13 +9,13 @@ export default function ManualSectionDropdown({ search }) {
       title: section.title,
       section: section.section,
       chapter: section.chapter,
-      body: section.content,
+      body: section.content, // markdown content
     }))
-    .sort((a, b) => a.chapter - b.chapter);
+    .sort((a, b) => a.section - b.section); // Sort by section number i.e 1-4
 
   return (
     <section className="px-6 py-12 max-w-7xl mx-auto">
-      {/* Radio inputs for each section */}
+      {/* Inputs for each section */}
       {sections.map((section) => (
         <input
           id={section.id}
@@ -27,8 +28,8 @@ export default function ManualSectionDropdown({ search }) {
         />
       ))}
 
-      {/* Styling for manual dropdown */}
-      <div className="flex flex-row bg-background-grey rounded shadow relative">
+      {/* Labels for inputs and styling for manual dropdown */}
+      <div className="flex flex-row rounded relative">
         {Object.entries(
           sections.reduce((acc, section) => {
             acc[section.chapter] = acc[section.chapter] || [];
@@ -37,13 +38,13 @@ export default function ManualSectionDropdown({ search }) {
           }, {}),
         ).map(([chapter, chapterSections]) => (
           <div className="relative group" key={`chapter-${chapter}`}>
-            <div className="cursor-pointer px-8 py-3 font-bold text-primary group-hover:bg-green group-hover:text-white">
-              {/* // FIXME: If section is 0 add the label htmlFor here */}
-              <label htmlFor={`${chapterSections[0].id}`}>
-                {Number(chapter) === 0 ? "Introduction" : `Chapter ${chapter}`}
-              </label>
-            </div>
-            <div className="absolute top-full left-0 mt-0 min-w-max hidden group-hover:flex flex-col bg-white rounded shadow-lg z-10">
+            <label
+              htmlFor={`${chapterSections[0].id}`}
+              className="cursor-pointer bg-background-grey px-8 py-3 font-bold text-primary group-hover:bg-green group-hover:text-white"
+            >
+              {Number(chapter) === 0 ? "Introduction" : `Chapter ${chapter}`}
+            </label>
+            <div className="absolute top-full left-0 mt-2 min-w-max hidden group-hover:flex flex-col bg-white rounded shadow-lg z-10">
               {chapterSections
                 .filter((section) => section.section !== 0) // Only include sections that are not 0
                 .map((section) => (
@@ -60,11 +61,11 @@ export default function ManualSectionDropdown({ search }) {
         ))}
       </div>
 
-      {/* Content Of Each Individual Section */}
+      {/* Content of each individual section */}
       {sections.map((section) => (
         <section
           key={`content-${section.id}`}
-          className={`overflow-hidden max-h-0 peer-checked/${section.id}:max-h-none peer-checked/${section.id}:py-6`}
+          className={`overflow-hidden max-h-0 peer-checked/${section.id}:max-h-none peer-checked/${section.id}:py-12`}
         >
           <ReactMarkdown className="prose max-w-none">
             {section.body}
