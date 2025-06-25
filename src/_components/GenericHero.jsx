5512c6354f21f6e90d331c335c1324d268eb73d8
Christian Apostol <christiand.apostol@outlook.com>
2025-06-25 00:02:05 -0400

    changed background color to grey

--- src/_components/GenericHero.jsx
+++ src/_components/GenericHero.jsx
@@ -1,6 +1,6 @@
 export default function GenericHero({ title, description, image }) {
   return (
-    <section className="grow flex space-between flex-col min-h-[600px] lg:flex-row lg:items-center relative overflow-hidden bg-primary text-white px-6 md:px-12 lg:px-32">
+    <section className="grow flex space-between flex-col min-h-[600px] lg:flex-row lg:items-center relative overflow-hidden bg-background-grey px-6 md:px-12 lg:px-32">
       <div className="py-8 md:py-16 lg:py-0 w-full lg:pr-16">
         <h1 className="font-bold text-2xl md:text-3xl lg:text-5xl leading-snug md:leading-[1.3] tracking-wide mb-4">
           {title}
