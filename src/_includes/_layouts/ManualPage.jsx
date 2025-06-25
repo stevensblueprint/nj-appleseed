3d40d558982b0a2cde6ac28b3cbc23e4165977ca
Christian Apostol <christiand.apostol@outlook.com>
2025-06-25 12:38:31 -0400

    attempt but error rendering content

--- src/_includes/_layouts/ManualPage.jsx
+++ src/_includes/_layouts/ManualPage.jsx
@@ -1,40 +1,52 @@
-export default function ManualPage({comp, title, chapter, section, content}) {
+export default function ManualPage({
+  comp,
+  title,
+  chapter,
+  section,
+  children,
+}) {
   // TODO: Use search to get and group each by chapter and section
-  // TODO: add an additional prop to .md metadata called url. 
+  // TODO: add an additional prop to .md metadata called url.
   // TODO: Key will be the chapter/section, value will be the URL to that section.
   // TODO: Create a sidebar similar to https://www.njcourts.gov/self-help/forms
   // const navigation = {
   //   "Chapter 1"
   // }
+
+  /**
+   * TODO: Have section title next to book. Almost like a hero. Introduction on the actual manual page with content component and sidebar. Each individual page will have a return to manual in its hero
+   */
   return (
     <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>{title}</title>
-      <link rel="stylesheet" href="/css/styles.css" />
-      {/* Google tag (gtag.js) */}
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-00LR9GZHMZ"
-      ></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{title}</title>
+        <link rel="stylesheet" href="/css/styles.css" />
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-00LR9GZHMZ"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
         function gtag() {
           dataLayer.push(arguments);
         }
         gtag("js", new Date());
         gtag("config", "G-00LR9GZHMZ");
         `,
-        }}
-      />
-    </head>
-    <body className="bg-gray-100 min-h-screen">
-      <comp.Header />
-      {content}
-      <comp.Footer />
-    </body>
-  </html>
-  )
+          }}
+        />
+      </head>
+      <body className="bg-gray-100 min-h-screen">
+        <comp.Header />
+        <div id="manual-content" className="max-w-7xl mx-auto px-6 py-12">
+          {children}
+        </div>
+        <comp.Footer />
+      </body>
+    </html>
+  );
 }
--- src/manual/chapter-1/ch1-s1.md
+++ src/manual/chapter-1/ch1-s1.md
@@ -9,6 +9,7 @@ indexable: true
 
 # Section 1: Understanding Your Plan—Co-pays, Co-insurance, Authorization, Appealing Denial of Benefits or Coverage
 
+
 If you have health insurance, you should have an ID card, which contains
 important information about your plan. This section will help you understand
 what the card says and what it means or coverage under your plan.
