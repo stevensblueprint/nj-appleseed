babdbcbee9eeced78877e4ecd50a184a63fa33ea
Johan Nguyen <thaithanhace@gmail.com>
2025-06-25 21:17:55 -0500

    done

--- src/_components/AddVideo.jsx
+++ src/_components/AddVideo.jsx
@@ -5,6 +5,7 @@ export default function AddVideo({ videos }) {
             {videos.map((video) => (
                 <div key={video.id} className="bg-white rounded-lg shadow-md p-6">
                     <h3 className="text-4xl text-dark-green font-bold mb-8">{video.header}</h3>
+                    <div className="aspect-w-16 aspect-h-9">
                         <iframe 
                             src={`https://www.youtube.com/embed/${video.link}`}
                             className="w-full h-96"
@@ -12,6 +13,7 @@ export default function AddVideo({ videos }) {
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
                         ></iframe>
+                    </div>
                 </div>
             ))}
         </div>
--- src/videos.md
+++ src/videos.md
@@ -2,6 +2,16 @@
 title: Videos
 layout: _layouts/Videos.jsx
 videos:
-  - header: "Understanding Medical Debt Basics"
+  - id: 1
+    header: "Header"
+    link: "dQw4w9WgXcQ"
+  - id: 2
+    header: "2"
+    link: "dQw4w9WgXcQ"
+  - id: 3
+    header: "3"
+    link: "dQw4w9WgXcQ"
+  - id: 4
+    header: "4"
     link: "dQw4w9WgXcQ"
 ---
