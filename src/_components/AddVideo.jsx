e4ccce6e3c74e7dbe2bbb1c6dd7ab9abb24d90f3
Johan Nguyen <thaithanhace@gmail.com>
2025-06-25 21:03:06 -0500

    allow user to create multiple videos

--- src/_components/AddVideo.jsx
+++ src/_components/AddVideo.jsx
@@ -1,13 +1,20 @@
 export default function AddVideo({ videos }) {
   return (
     <section className="max-w-6xl mx-auto px-4 py-12">
-      <iframe
-        src={`https://www.youtube.com/embed/${videos.link}`}
-        className="w-full h-96"
-        title={videos.header}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+        <div className="space-y-8">
+            {videos.map((video) => (
+                <div>
+                    <iframe 
+                        src={`https://www.youtube.com/embed/${video.link}`}
+                        className="w-full h-96"
+                        title={video.header}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+                </div>
+            ))}
+        </div>
     </section>
   );
 }
+
